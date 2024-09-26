343d1e4d0cb3b1c34b6e7fdcbd2107a281eb44eb
Francisco0527 <franciscojff7@gmail.com>
2024-09-26 15:57:57 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="logo" src="./assets/logo.png" />
+    <h1>Lugares</h1>
+    <Menu @city-selected="city = $event" />
+    <component :is="currentCityComponent"></component>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from "./components/Menu.vue";
+import Paraty from "./components/Paraty.vue";
+import Campos from "./components/Campos.vue";
+import Gramado from "./components/Gramado.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Menu,
+    Paraty,
+    Campos,
+    Gramado,
+  },
+  data() {
+    return {
+      city: "Cidades", // Cidade padrão exibida ao iniciar
+    };
+  },
+  computed: {
+    currentCityComponent() {
+      return this.city;
+    },
+  },
+};
 </script>
-
 <style>
-#app {
+* {
+  padding: 0;
+  margin: 0;
+}
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-image: url("/src/assets/background.jpg");
+  background-size: cover;
+  background-position: 0;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  height: 100vh;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #d4d2d2;
+  /* overflow: hidden; */
+}
+</style>
+<style scoped>
+img {
+  height: 80px;
+  margin-top: 5px;
+}
+h1 {
+  font-size: 36px;
 }
 </style>
