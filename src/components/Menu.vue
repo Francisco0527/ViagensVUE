3b46299096b5913bdbcf11e954ec2e9fe7a41777
Francisco0527 <franciscojff7@gmail.com>
2024-09-26 15:58:16 -0300

    Create Menu.vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="dropdown">
+    <select v-model="selectedCity" @change="updateCity">
+      <option value="Cidades" selected>Cidades</option>
+      <option value="Paraty">Paraty</option>
+      <option value="Campos">Campos do Jordão</option>
+      <option value="Gramado">Gramado</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedCity: "Cidades", // Cidade inicial nula
+    };
+  },
+  methods: {
+    updateCity() {
+      this.$emit("city-selected", this.selectedCity);
+    },
+  },
+};
+</script>
+
+<style>
+.dropdown select {
+  background-color: inherit;
+  color: #ccc;
+  padding: 10px;
+  font-size: 28px;
+  margin: 10px auto;
+  display: block;
+  border: none;
+  cursor: pointer;
+}
+.dropdown select option {
+  font-size: 16px;
+}
+</style>
